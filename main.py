e92a850f7cadebce1b9ed53ddc5aeb8d6bceda38
Pratyush Tiwari <125774489+tiwariPratyush@users.noreply.github.com>
2025-11-29 15:44:41 +0530

    added get request for patient retrieval by P_ID

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 import json
 
 app = FastAPI()
@@ -20,3 +20,12 @@ def about():
 def view():
     data = load_data()
     return data
+
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id: str=Path(..., description="ID of the patient in the DB",example="P001")):
+    # load all patients data
+    data = load_data()
+    
+    if patient_id in data:
+        return data[patient_id]
+    return {"Error": "Patient not found."}
