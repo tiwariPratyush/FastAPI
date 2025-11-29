34bad412a006cd7cff6d9ef1913aa89c73f78d1e
Pratyush Tiwari <125774489+tiwariPratyush@users.noreply.github.com>
2025-11-29 15:29:17 +0530

    added one more get api and data

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+def load_data(): 
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+    return data
+
 @app.get("/")
 def hello():
-    return {"message": "Hello, World!"}
+    return {"Message": "Patient Management System API"}
 
 @app.get("/about")
 def about():
-    return {"Hi, Pratyush this side. Trying to learn FastAPI!"}
+    return {"Message": "This API is designed to manage patient records efficiently."} 
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
