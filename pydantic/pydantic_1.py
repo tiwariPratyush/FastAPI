e78a65174876ceb113f6b1d111bac0d0f14f1139
Pratyush Tiwari <125774489+tiwariPratyush@users.noreply.github.com>
2025-12-01 00:57:38 +0530

    used typing and basemodel modules for creatig a pydantic model and giving cutom constraints

--- pydantic/pydantic_1.py
+++ pydantic/pydantic_1.py
@@ -1,19 +1,23 @@
-from pydantic import BaseModel  # pydantic is used for data validation and settings management using python type annotations
-from typing import List , Dict 
+from pydantic import BaseModel, EmailStr,Field  # pydantic is used for data validation and settings management using python type annotations
+from typing import List , Dict, Optional, Annotated 
 
 
 class Patient(BaseModel):
     
-    name : str
-    age : int
-    weight : float
-    married : bool
-    allergies : List[str]  # only list is not sufficient we need to specify the type of elements in the list
+    # name : str = Field(max_length=50) # name should not exceed 50 characters
+    name : Annotated[str,Field(max_length=50,title="Name of the Patient",description="This field is used to store the name of the patient",examples = ["Pratyush","Saurabh"])] # using annotated to add more metadata
+    email : EmailStr
+    age : int = Field(gt=0,lt=60) # age should be greater than 0 and less than 60
+    weight : float = Field(gt=0) # weight should be greater than 0
+    # married : bool
+    married : Annotated[bool,Field(default=None,title="Marital Status",description="This field indicates whether the patient is married or not",examples=[True,False])] # using annotated to add more metadata
+    allergies : Optional[List[str]] = None  # only list is not sufficient we need to specify the type of elements in the list
     contact_details : Dict[str,str] # only dict is not sufficient we need to specify the type of keys and values in the dict
     
     
 def insert_patient_data(patient:Patient):
     print(patient.name)
+    print(patient.email)
     print(patient.age)
     print(patient.weight)
     print(patient.married)
@@ -23,6 +27,7 @@ def insert_patient_data(patient:Patient):
     
 def update_patient_data(patient:Patient):
     print(patient.name)
+    print(patient.email)
     print(patient.age)
     print(patient.weight)
     print(patient.married)
@@ -31,7 +36,7 @@ def update_patient_data(patient:Patient):
     print("updated" )
     
     
-patient_info = {"name":"Pratyush","age":21,"weight":90,"married":False,"allergies":["pollen","dust"],"contact_details":{"email":"pratyush@google.com","phone":"9129078650"}}
+patient_info = {"name":"Pratyush","email":"pratyush@gmail.com","age":21,"weight":90,"married":False,"contact_details":{"phone":"9129078650"}}
 
 
 patient1 = Patient(**patient_info) # ** to unpack the dictionary
